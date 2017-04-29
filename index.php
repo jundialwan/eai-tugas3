bfb94a6d26ee9dacb08f665777ab30d3221e30af
jundialwan <jundi.alwan@gmail.com>
2017-04-29 15:44:14 +0700

    refactoring code and make it more efficient

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
+/**
+ * Assignment 3 - EAI
+ * Web Scrapper
+ * 
+ * 1306382852 - Hari
+ * 1306464114 - Jundi Ahmad Alwan
+ * 1306404645 - Muhammad Azki Darmawan
+ * 
+ * github: https://github.com/jundialwan/eai-tugas3
+ *  
+ */
 
 include 'simple_html_dom.php';
 
 function getHtml($query)
 {
-    $opt = [
-        'http' => [
-            'header' => 'User-Agent:MyAgent/1.0\r\n'
-        ]
-    ];
-
-    $ctx = stream_context_create($opt);
+    $ctx = stream_context_create(['http' => ['header' => 'User-Agent:MyAgent/1.0\r\n']]);
     $header = file_get_contents('http://www.unsri.ac.id/'.$query, false, $ctx);
 
     $html = new simple_html_dom();
@@ -19,10 +24,7 @@ function getHtml($query)
     return $html;
 }
 
-// first table > tbody > second tr > first td > div > div#content-front-left > div > table > tbody > second tr > td > ul > li > a[href]
-
 $faculties_html = getHtml('?act=daftar_dosen');
-
 $links = $faculties_html->find('a[href^="?act=daftar_dosen&fakultas="]');
 
 $csv = fopen('database.csv', 'w');
@@ -36,23 +38,18 @@ foreach ($links as $l)
     $faculty = trim(str_replace('DAFTAR DOSEN ', '', $lecturer_html->find('table', 1)->find('tr', 0)->find('td h3', 0)->innertext));
 
     for ($i=1; $i < count($table->find('tr')); $i++)
-    {
-        $nip = $nik = '';
+    {        
+        $row = $table->find('tr', $i);            
+        $number = trim($row->find('td', 2)->innertext);
 
-        $row = $table->find('tr', $i);        
-
-        $nama = trim($row->find('td', 3)->innertext);
-        $number = trim($row->find('td', 2)->innertext);        
-        
-        if ($number[0].$number[1] == '19')
-            $nip = $number;
-        else
-            $nik = $number;
-
-        $jabatan = trim($row->find('td', 4)->innertext);
-        $prodi = trim($row->find('td', 5)->innertext);
-
-        fputcsv($csv, [$faculty, $nip, $nik, $nama, $jabatan, $prodi]);
+        fputcsv($csv, [
+            $faculty, // fakultas 
+            ($number[0].$number[1] == '19') ? $number : '', // nip
+            ($number[0].$number[1] != '19') ? $number : '', // nik
+            trim($row->find('td', 3)->innertext), // nama
+            trim($row->find('td', 4)->innertext), // jabatan
+            trim($row->find('td', 5)->innertext)  // prodi
+        ]);
     }
 }
 
